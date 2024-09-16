29d3715a1b00683a9febb3d0a9f8cb078f02379a
Marcos Avila <marcos.seocuba@gmail.com>
2024-09-15 23:39:24 -0400

    "Added asynccontextmanager for database setup and teardown, and updated FastAPI app initialization to include lifespan parameter."

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.db.engine import engine
+from app.models.models import Base
+
+
+@asynccontextmanager
+def lifespan():
+    Base.metadata.create_all(engine)
+    yield
+    pass
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
