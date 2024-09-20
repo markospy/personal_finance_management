39b65b9a9c3dd58ecbc4a0429776119bf2750dd7
Marcos Avila <marcos.seocuba@gmail.com>
2024-09-20 09:31:25 -0400

    "Updated model definitions: changed data types, added foreign keys, and modified relationships."

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import List
 
-from sqlalchemy import JSON, DateTime, Enum, Float, Integer, String
+from sqlalchemy import JSON, ForeignKey, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 from ..schemas.schemas import Frecuency, TransactionType
@@ -14,42 +14,44 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "user"
 
-    id: Mapped[str] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(50))
     password: Mapped[str] = mapped_column(String(50))
 
-    account: Mapped[List["Account"]] = relationship(back_populates="user")
-    expected_transaction: Mapped[List["ExpectedTransaction"]] = relationship(back_populates="user")
-    budget: Mapped[List["Budget"]] = relationship(back_populates="user")
+    account: Mapped[List["Account"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+    expected_transaction: Mapped[List["ExpectedTransaction"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+    budget: Mapped[List["Budget"]] = relationship(back_populates="user", cascade="all, delete-orphan")
 
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+        return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r})"
 
 
 class Account(Base):
     __tablename__ = "account"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     currency: Mapped[str] = mapped_column(String(20))
-    amount: Mapped[float] = mapped_column(Float)
+    amount: Mapped[float]
     name: Mapped[str] = mapped_column(String(50))
 
     user: Mapped["User"] = relationship(back_populates="account")
-    transaction: Mapped[List["Transaction"]] = relationship(back_populates="account")
+    transaction: Mapped[List["Transaction"]] = relationship(back_populates="account", cascade="all, delete-orphan")
 
 
 class ExpectedTransaction(Base):
     __tablename__ = "expected_transaction"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    category_id: Mapped[int] = mapped_column(Integer)
-    user_id: Mapped[int] = mapped_column(Integer)
-    amount: Mapped[float] = mapped_column(Float)
-    frequency: Mapped[Frecuency] = mapped_column(Enum(Frecuency))
-    date: Mapped[datetime] = mapped_column(DateTime)
-    type: Mapped[TransactionType] = mapped_column(Enum(TransactionType))
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    amount: Mapped[float]
+    frequency: Mapped[Frecuency]
+    date: Mapped[datetime]
+    type: Mapped[TransactionType]
 
     user: Mapped["User"] = relationship(back_populates="expected_transaction")
     category: Mapped["Category"] = relationship(back_populates="expected_transaction")
@@ -58,22 +60,24 @@ class ExpectedTransaction(Base):
 class Category(Base):
     __tablename__ = "category"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(50))
-    type: Mapped[TransactionType] = mapped_column(Enum(TransactionType))
+    type: Mapped[TransactionType]
 
-    expected_transaction: Mapped[List["ExpectedTransaction"]] = relationship(back_populates="category")
-    budget: Mapped[List["Budget"]] = relationship(back_populates="category")
-    transaction: Mapped[List["Transaction"]] = relationship(back_populates="category")
+    expected_transaction: Mapped[List["ExpectedTransaction"]] = relationship(
+        back_populates="category", cascade="all, delete-orphan"
+    )
+    budget: Mapped[List["Budget"]] = relationship(back_populates="category", cascade="all, delete-orphan")
+    transaction: Mapped[List["Transaction"]] = relationship(back_populates="category", cascade="all, delete-orphan")
 
 
 class Budget(Base):
     __tablename__ = "budget"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    category_id: Mapped[int] = mapped_column(Integer)
-    user_id: Mapped[int] = mapped_column(Integer)
-    amount: Mapped[float] = mapped_column(Float)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    amount: Mapped[float]
     period: Mapped[dict] = mapped_column(JSON)
 
     user: Mapped["User"] = relationship(back_populates="budget")
@@ -83,13 +87,13 @@ class Budget(Base):
 class Transaction(Base):
     __tablename__ = "transaction"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    category_id: Mapped[int] = mapped_column(Integer)
-    account_id: Mapped[int] = mapped_column(Integer)
-    amount: Mapped[float] = mapped_column(Float)
-    date: Mapped[datetime] = mapped_column(DateTime)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"))
+    account_id: Mapped[int] = mapped_column(ForeignKey("account.id"))
+    amount: Mapped[float]
+    date: Mapped[datetime]
     comments: Mapped[str] = mapped_column(String(250))
-    type: Mapped[TransactionType] = mapped_column(Enum(TransactionType))
+    type: Mapped[TransactionType]
 
     category: Mapped["Category"] = relationship(back_populates="transaction")
     account: Mapped["Account"] = relationship(back_populates="transaction")
