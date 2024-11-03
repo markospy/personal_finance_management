3d8dfdd2bbe13e30e4bf895ae9b3c3642e4fb144
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-02 21:36:31 -0400

    ☑️ Updated user schema with required error messages and added userUpdate schema

--- frontend/web/app/src/schemas/user.ts
+++ frontend/web/app/src/schemas/user.ts
@@ -2,15 +2,15 @@ import { z } from "zod";
 
 const userBase = z.object(
   {
-    "name": z.string().min(2, 'Must be 2 or more characters long').max(50, 'Must be 50 or fewer characters long'),
-    "email": z.string().email(),
+    "name": z.string({required_error: "Name is required"}).min(2, 'Must be 2 or more characters long').max(50, 'Must be 50 or fewer characters long'),
+    "email": z.string({required_error: "Email is required"}).email(),
   }
 )
 
 const userIn = userBase.merge(
   z.object(
     {
-      "password": z.string().min(6, 'Must be 6 or more characters long')
+      "password": z.string({required_error: "Name is required"}).min(6, 'Must be 6 or more characters long')
     }
   )
 )
@@ -23,5 +23,15 @@ const userOut = userBase.merge(
   )
 )
 
+const userUpdate = z.object(
+  {
+    "name": z.string().min(2, 'Must be 2 or more characters long').max(50, 'Must be 50 or fewer characters long').optional(),
+    "email": z.string().email().optional(),
+    "password": z.string({required_error: "Name is required"}).min(6, 'Must be 6 or more characters long').optional()
+  }
+)
+
+
 export type userIn = z.infer<typeof userIn>
 export type userOut = z.infer<typeof userOut>
+export type userUpdate = z.infer<typeof userUpdate>
