0a8f79cecaadaf4ca00733a73fcd5c06bd86dc9e
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-03 20:13:28 -0500

    💡 "Added deleteCategory function and error handling to getCategory function in category.ts"

--- frontend/web/app/src/api/category.ts
+++ frontend/web/app/src/api/category.ts
@@ -97,3 +97,30 @@ export const getCategory: (token: string, id: number) => Promise<CategoryOut> = 
 		return Promise.reject(error);
 	});
 }
+
+
+export const deleteCategory: (token: string, id: number, type: CategoryType,) => Promise<string> = (token, id, type) => {
+
+	return axi.delete(`/categories/${id}/${type}`, {headers: {'Authorization': `Bearer ${token}`}})
+	.then( response =>
+		{
+			console.log(response.status)
+			return response.statusText;
+		}
+	).catch(error => {
+		// Manejo de errores
+		if (error.response) {
+			console.log('Error en la respuesta del servidor:', error.response.data);
+			console.log('Estado del error:', error.response.status);
+			console.log('Encabezados del error:', error.response.headers);
+		} else if (error.request) {
+			console.log('No se recibió respuesta:', error.request);
+		} else {
+			console.log('Error al configurar la solicitud:', error.message);
+		}
+		console.log('Configuración de la solicitud:', error.config);
+
+		// Puedes lanzar el error de nuevo si quieres que la promesa se rechace
+		return Promise.reject(error);
+	});
+}
