a8b2031df434c8384cebbab8777c44371f843f35
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-04 13:18:08 -0500

    🐞 "Added new API function getTransactionsByAccount and updated error handling in getTransactions"

--- frontend/web/app/src/api/transactions.ts
+++ frontend/web/app/src/api/transactions.ts
@@ -87,3 +87,31 @@ export const getTransactions: (toke: string) => Promise<TransactionOut[]> = (tok
 		return Promise.reject(error);
 	});
 }
+
+export const getTransactionsByAccount: (toke: string, accountId: number) => Promise<TransactionOut[]> = (token, accountId) => {
+	return axi.get('/transactions/account/'+ accountId,
+		{headers: {'Authorization': `Bearer ${token}`}}
+	)
+	.then(
+		response => {
+			console.log(response.status)
+			return response.data;
+		}
+	)
+	.catch(error => {
+		// Manejo de errores
+		if (error.response) {
+			console.log('Error en la respuesta del servidor:', error.response.data);
+			console.log('Estado del error:', error.response.status);
+			console.log('Encabezados del error:', error.response.headers);
+		} else if (error.request) {
+			console.log('No se recibió respuesta:', error.request);
+		} else {
+			console.log('Error al configurar la solicitud:', error.message);
+		}
+		console.log('Configuración de la solicitud:', error.config);
+
+		// Puedes lanzar el error de nuevo si quieres que la promesa se rechace
+		return Promise.reject(error);
+	});
+}
