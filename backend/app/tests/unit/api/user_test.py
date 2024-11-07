ecd29acafa4d38549f6c9bb501eff17371c58024
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-06 21:25:35 -0500

    👩 "Rename fixture 'client_some' to 'unregistered_user', update test data, and rename some test clients and tokens."

--- backend/app/tests/unit/api/user_test.py
+++ backend/app/tests/unit/api/user_test.py
@@ -5,7 +5,7 @@ from ....main import app
 
 
 @pytest.fixture
-def client_some():
+def unregistered_user():
     client = TestClient(app)
     return client
 
@@ -14,54 +14,50 @@ def client_some():
 @pytest.mark.create_user
 class TestUserCreation:
 
-    def test_create_user(self, client_some: TestClient):
-        response = client_some.post(
+    def test_create_user(self, unregistered_user: TestClient):
+        response = unregistered_user.post(
             "/user/",
-            json={"name": "Jane Doe", "email": "john@gmail.com", "password": "123456"},
+            json={"name": "John Doe", "email": "john@gmail.com", "password": "123456"},
         )
         assert response.status_code == 201
-        assert response.json() == {"id": 1, "name": "Jane Doe", "email": "john@gmail.com"}
+        assert response.json() == {"id": 1, "name": "John Doe", "email": "john@gmail.com"}
 
-    def test_create_user_non_existent_name(self, client_some: TestClient):
-        response = client_some.post(
+    def test_create_user_already_exists(self, unregistered_user: TestClient):
+        response = unregistered_user.post(
             "/user/",
-            json={"email": "john@gmail.com", "password": "123456"},
-        )
-        assert response.status_code == 422
-        assert response.json()["detail"][0]["msg"] == "Field required"
-
-    def test_create_user_already_exists(self, client_some: TestClient):
-        response = client_some.post(
-            "/user/",
-            json={"name": "Jane Doe", "email": "john@gmail.com", "password": "123456"},
+            json={"name": "John Doe", "email": "john@gmail.com", "password": "123456"},
         )
         assert response.status_code == 201
 
-        response = client_some.post(
+        response = unregistered_user.post(
             "/user/",
-            json={"name": "Jane Doe", "email": "john@gmail.com", "password": "123456"},
+            json={"name": "John Doe", "email": "john@gmail.com", "password": "123456"},
         )
 
         assert response.status_code == 409
         assert response.json()["detail"] == "The user's name is already being used by another user"
 
 
+@pytest.mark.user
+@pytest.mark.get_user
 class TestUserGet:
 
-    def test_user_name(self, client: TestClient, token_user: dict):
-        response = client.get("/user/me", headers=token_user)
+    def test_user_name(self, client_John: TestClient, John_token: dict):
+        response = client_John.get("/user/me", headers=John_token)
         assert response.status_code == 200
-        assert response.json()["name"] == client.user["name"]
+        assert response.json()["name"] == client_John.user["name"]
 
 
+@pytest.mark.user
+@pytest.mark.delete_user
 class TestUserDelete:
 
-    def test_delete_me(self, client: TestClient, token_user: dict):
+    def test_delete_me(self, client_John: TestClient, John_token: dict):
 
         # Delete the user
-        response = client.delete("/user/me", headers=token_user)
+        response = client_John.delete("/user/me", headers=John_token)
         assert response.status_code == 204
 
         # Try to get the user again, should return 404
-        response = client.get("/user/me", headers=token_user)
+        response = client_John.get("/user/me", headers=John_token)
         assert response.status_code == 404
