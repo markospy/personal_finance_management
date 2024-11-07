56a0d73b578cc51134e7d2c808fb0651d4afc8eb
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-06 21:27:40 -0500

    🔨 "Refactored budget API tests to use new fixtures and updated test cases"

--- backend/app/tests/unit/api/budget_test.py
+++ backend/app/tests/unit/api/budget_test.py
@@ -1,57 +1,28 @@
 from datetime import datetime, timedelta
+from random import randint
 
 import pytest
 from fastapi.testclient import TestClient
-
-
-@pytest.fixture
-def create_categories(client: TestClient, token_admin: dict, token_user: dict):
-    # Crear categorías de prueba
-    global_category = client.post(
-        "/categories/global",
-        headers=token_admin,
-        json={"name": "Categoría global", "type": "expense"},
-    )
-    assert global_category.status_code == 201
-
-    user_category = client.post(
-        "/categories/user",
-        headers=token_user,
-        json={
-            "name": "Categoría de usuario",
-            "type": "expense",
-            "is_global": False,
-        },
-    )
-    assert user_category.status_code == 201
-    return (global_category.json()["id"], user_category.json()["id"])
-
-
-@pytest.fixture
-def create_account(client: TestClient, token_user: dict):
-    # Crear una cuenta de prueba para el usuario
-    response = client.post(
-        "/accounts/",
-        headers=token_user,
-        json={"name": "Cuenta Ahorros", "currency": "USD", "balance": 5000},
-    )
-    assert response.status_code == 201
-    return response.json()["id"]
 
 
 @pytest.mark.budget
 @pytest.mark.create_budget
 class TestBudgetCreation:
 
-    def test_create_budget(self, client: TestClient, token_user: dict, create_categories: tuple[int]):
+    def test_create_budget(
+        self,
+        client_John: TestClient,
+        John_token: dict,
+        global_expense_category: dict,
+    ):
         # Crear un presupuesto válido
         start_date = datetime.now()
         end_date = start_date + timedelta(days=30)
-        response = client.post(
+        response = client_John.post(
             "/budgets/",
-            headers=token_user,
+            headers=John_token,
             json={
-                "category_id": create_categories[0],
+                "category_id": global_expense_category["id"],
                 "amount": 1000.0,
                 "period": {
                     "start_date": str(start_date),
@@ -61,37 +32,20 @@ class TestBudgetCreation:
         )
         assert response.status_code == 201
         budget = response.json()
-        assert budget["category_id"] == create_categories[0]
+        assert budget["id"] == 1
+        assert budget["user_id"] == client_John.user["id"]
+        assert budget["category_id"] == global_expense_category["id"]
         assert budget["amount"] == 1000.0
         assert "start_date" in budget["period"]
         assert "end_date" in budget["period"]
 
-    def test_create_budget_invalid_dates(self, client: TestClient, token_user: dict, create_categories: tuple[int]):
-        # Intentar crear un presupuesto con fechas inválidas (end_date antes que start_date)
-        start_date = datetime.now()
-        end_date = start_date - timedelta(days=30)
-        response = client.post(
-            "/budgets/",
-            headers=token_user,
-            json={
-                "category_id": create_categories[0],
-                "amount": 1000.0,
-                "period": {
-                    "start_date": str(start_date),
-                    "end_date": str(end_date),
-                },
-            },
-        )
-        assert response.status_code == 422
-        assert response.json()["detail"][0]["msg"] == "Value error, Start date must be before end date"
-
-    def test_create_budget_invalid_category(self, client: TestClient, token_user: dict):
+    def test_create_budget_invalid_category(self, client_John: TestClient, John_token: dict):
         # Intentar crear un presupuesto con una categoría inexistente
         start_date = datetime.now()
         end_date = start_date + timedelta(days=30)
-        response = client.post(
+        response = client_John.post(
             "/budgets/",
-            headers=token_user,
+            headers=John_token,
             json={
                 "category_id": 99999,
                 "amount": 1000.0,
@@ -106,51 +60,19 @@ class TestBudgetCreation:
 
     def test_get_budget_status(
         self,
-        client: TestClient,
-        token_user: dict,
-        create_account: int,
-        create_categories: tuple[int],
+        client_John: TestClient,
+        John_token: dict,
+        budget_John: dict,
+        expense_transaction_John: dict,
     ):
-        # Crear un presupuesto válido
-        start_date = datetime.now()
-        end_date = start_date + timedelta(days=30)
-        response = client.post(
-            "/budgets/",
-            headers=token_user,
-            json={
-                "category_id": create_categories[0],
-                "amount": 1000.0,
-                "period": {
-                    "start_date": str(start_date),
-                    "end_date": str(end_date),
-                },
-            },
-        )
-        assert response.status_code == 201
-        budget_id = response.json()["id"]
-
-        # Crear algunas transacciones para simular gastos
-        date = str(datetime.now())
-        response = client.post(
-            "/transactions",
-            headers=token_user,
-            json={
-                "category_id": create_categories[0],
-                "account_id": create_account,
-                "amount": 200.0,
-                "date": date,
-                "comments": "Pago de servicios",
-            },
-        )
-        assert response.status_code == 201
 
         # Consultar el estado de los presupuestos
-        response = client.get(f"/budgets/{budget_id}/status", headers=token_user)
+        response = client_John.get(f"/budgets/{budget_John["id"]}/status", headers=John_token)
         assert response.status_code == 200
         data = response.json()
 
         # Verificar la respuesta
-        assert data["category_id"] == budget_id
+        assert data["category_id"] == budget_John["category_id"]
         assert data["budget_amount"] == 1000.0
         assert data["spent_amount"] == 200.0
         assert data["remaining_amount"] == 800.0
@@ -161,17 +83,23 @@ class TestBudgetCreation:
 @pytest.mark.get_budget
 class TestGetBudget:
 
-    def test_get_all_budgets(self, client: TestClient, token_user: dict, create_categories: tuple[int]):
+    def test_get_all_budgets(
+        self,
+        client_John: TestClient,
+        John_token: dict,
+        global_expense_category: dict,
+        John_income_category: dict,
+    ):
         # Crear varios presupuestos
-        for i in range(2):
+        for category in [global_expense_category, John_income_category]:
             start_date = datetime.now()
             end_date = start_date + timedelta(days=30)
-            response = client.post(
+            response = client_John.post(
                 "/budgets/",
-                headers=token_user,
+                headers=John_token,
                 json={
-                    "category_id": create_categories[i - 1],
-                    "amount": 1000.0 + i * 500,  # Diferentes montos para cada presupuesto
+                    "category_id": category["id"],
+                    "amount": 1000.0 + randint(0, 1000),  # Diferentes montos para cada presupuesto
                     "period": {
                         "start_date": str(start_date),
                         "end_date": str(end_date),
@@ -181,61 +109,46 @@ class TestGetBudget:
             assert response.status_code == 201
 
         # Obtener todos los presupuestos
-        response_get = client.get("/budgets/", headers=token_user)
+        response_get = client_John.get("/budgets/", headers=John_token)
         assert response_get.status_code == 200
         budgets = response_get.json()
         assert isinstance(budgets, list)  # Verificar que la respuesta sea una lista
-        assert len(budgets) == 2  # Verificar que se hayan creado 3 presupuestos
+        assert len(budgets) == 2  # Verificar que se hayan creado 2 presupuestos
 
         # Verificar que cada presupuesto tenga los campos esperados
         for budget in budgets:
             assert "id" in budget
+            assert "user_id" in budget
             assert "category_id" in budget
             assert "amount" in budget
             assert "period" in budget
             assert "start_date" in budget["period"]
             assert "end_date" in budget["period"]
 
-    def test_get_all_budgets_not_found(self, client: TestClient, token_user: dict):
-        response = client.get("/budgets/", headers=token_user)
+    def test_get_all_budgets_not_found(self, client_John: TestClient, John_token: dict):
+        response = client_John.get("/budgets/", headers=John_token)
         assert response.status_code == 404
         assert response.json()["detail"] == "Budgets is not found"
 
     def test_get_one_budget(
         self,
-        client: TestClient,
-        token_user: dict,
-        create_categories: tuple[int],
+        client_John: TestClient,
+        John_token: dict,
+        global_expense_category: dict,
+        budget_John: dict,
     ):
-        # Crear un presupuesto válido
-        start_date = datetime.now()
-        end_date = start_date + timedelta(days=30)
-        response = client.post(
-            "/budgets/",
-            headers=token_user,
-            json={
-                "category_id": create_categories[0],
-                "amount": 1000.0,
-                "period": {
-                    "start_date": str(start_date),
-                    "end_date": str(end_date),
-                },
-            },
-        )
-        assert response.status_code == 201
-        budget_post = response.json()
-
-        response_budget = client.get("/budgets/1", headers=token_user)
+        response_budget = client_John.get(f"/budgets/{budget_John["id"]}", headers=John_token)
         budget = response_budget.json()
         assert response_budget.status_code == 200
-        assert budget["id"] == budget_post["id"]
-        assert budget["category_id"] == budget_post["category_id"]
-        assert budget["amount"] == budget_post["amount"]
-        assert budget["period"]["start_date"] in budget_post["period"]["start_date"]
-        assert budget["period"]["end_date"] in budget_post["period"]["end_date"]
+        assert budget["id"] == 1
+        assert budget["user_id"] == client_John.user["id"]
+        assert budget["category_id"] == global_expense_category["id"]
+        assert budget["amount"] == 1000
+        assert budget["period"]["start_date"] == budget_John["period"]["start_date"]
+        assert budget["period"]["end_date"] == budget_John["period"]["end_date"]
 
-    def test_get_one_budget_not_found(self, client: TestClient, token_user: dict):
-        response = client.get("/budgets/1", headers=token_user)
+    def test_get_one_budget_not_found(self, client_John: TestClient, John_token: dict):
+        response = client_John.get("/budgets/1", headers=John_token)
         assert response.status_code == 404
         assert response.json()["detail"] == "Budgets is not found"
 
@@ -244,33 +157,22 @@ class TestGetBudget:
 @pytest.mark.update_budget
 class TestUpdateBudget:
 
-    def test_update_budget(self, client: TestClient, token_user: dict, create_categories: tuple[int]):
-        # Crear un presupuesto válido
-        start_date = datetime.now()
-        end_date = start_date + timedelta(days=30)
-        response = client.post(
-            "/budgets/",
-            headers=token_user,
-            json={
-                "category_id": create_categories[0],
-                "amount": 1000.0,
-                "period": {
-                    "start_date": str(start_date),
-                    "end_date": str(end_date),
-                },
-            },
-        )
-        assert response.status_code == 201
-        budget_post = response.json()
+    def test_update_budget(
+        self,
+        client_John: TestClient,
+        John_token: dict,
+        income_transaction_John: dict,
+        budget_John: dict,
+    ):
 
         # Actualizar el presupuesto creado
         new_start_date = datetime.now() + timedelta(days=1)
         new_end_date = new_start_date + timedelta(days=29)
-        response_update = client.put(
-            f"/budgets/{budget_post['id']}",
-            headers=token_user,
+        response_update = client_John.put(
+            f"/budgets/{budget_John['id']}",
+            headers=John_token,
             json={
-                "category_id": create_categories[0],
+                "category_id": income_transaction_John["id"],
                 "amount": 1500.0,
                 "period": {
                     "start_date": str(new_start_date),
@@ -280,15 +182,18 @@ class TestUpdateBudget:
         )
         assert response_update.status_code == 201
         updated_budget = response_update.json()
+        assert updated_budget["id"] == 1
+        assert updated_budget["category_id"] == income_transaction_John["id"]
+        assert updated_budget["user_id"] == client_John.user["id"]
         assert updated_budget["amount"] == 1500.0
         assert updated_budget["period"]["start_date"] == str(new_start_date.isoformat())
         assert updated_budget["period"]["end_date"] == str(new_end_date.isoformat())
 
-    def test_update_budget_not_found(self, client: TestClient, token_user: dict):
+    def test_update_budget_not_found(self, client_John: TestClient, John_token: dict):
         # Intentar actualizar un presupuesto inexistente
-        response = client.put(
+        response = client_John.put(
             "/budgets/99999",
-            headers=token_user,
+            headers=John_token,
             json={
                 "category_id": 1,
                 "amount": 1000.0,
@@ -303,36 +208,24 @@ class TestUpdateBudget:
 @pytest.mark.delete_budget
 class TestDeleteBudget:
 
-    def test_delete_budget(self, client: TestClient, token_user: dict, create_categories: tuple[int]):
-        # Crear un presupuesto válido
-        start_date = datetime.now()
-        end_date = start_date + timedelta(days=30)
-        response = client.post(
-            "/budgets/",
-            headers=token_user,
-            json={
-                "category_id": create_categories[0],
-                "amount": 1000.0,
-                "period": {
-                    "start_date": str(start_date),
-                    "end_date": str(end_date),
-                },
-            },
-        )
-        assert response.status_code == 201
-        budget_post = response.json()
+    def test_delete_budget(
+        self,
+        client_John: TestClient,
+        John_token: dict,
+        budget_John: dict,
+    ):
 
         # Eliminar el presupuesto creado
-        response_delete = client.delete(f"/budgets/{budget_post['id']}", headers=token_user)
+        response_delete = client_John.delete(f"/budgets/{budget_John['id']}", headers=John_token)
         assert response_delete.status_code == 204
 
         # Intentar obtener el presupuesto eliminado
-        response_get = client.get(f"/budgets/{budget_post['id']}", headers=token_user)
+        response_get = client_John.get(f"/budgets/{budget_John['id']}", headers=John_token)
         assert response_get.status_code == 404
         assert response_get.json()["detail"] == "Budgets is not found"
 
-    def test_delete_budget_not_found(self, client: TestClient, token_user: dict):
+    def test_delete_budget_not_found(self, client_John: TestClient, John_token: dict):
         # Intentar eliminar un presupuesto inexistente
-        response = client.delete("/budgets/99999", headers=token_user)
+        response = client_John.delete("/budgets/99999", headers=John_token)
         assert response.status_code == 404
         assert response.json()["detail"] == "Budgets is not found"
