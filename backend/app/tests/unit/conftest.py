6014fbdc1488652b4f7ca422032eb86a7562a21f
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-06 21:24:41 -0500

    ⏰ "Updated conftest.py with new fixtures and renamed existing ones"

--- backend/app/tests/unit/conftest.py
+++ backend/app/tests/unit/conftest.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event, select
@@ -10,7 +12,7 @@ from ...main import app
 
 # from app import settings
 from ...models.models import Base, User
-from ...schemas.schemas import UserIn, UserOut
+from ...schemas.schemas import TransactionType, UserIn, UserOut
 
 
 @pytest.fixture(scope="session")
@@ -61,7 +63,7 @@ def db(engine: Engine, request):
 
 
 @pytest.fixture
-def user(db: Session):
+def John(db: Session):
     user = UserIn(name="John Doe", email="john@gmail.com", password="123456")
     new_user = user.model_dump(exclude_unset=True)
     db.add(User(**new_user))
@@ -73,28 +75,28 @@ def user(db: Session):
 
 
 @pytest.fixture
-def client(user):
+def client_John(John):
     client = TestClient(app)
-    client.user = user
+    client.user = John
     return client
 
 
 @pytest.fixture
-def token_user(client):
-    name = {"sub": client.user["name"], "scopes": ["user"]}
+def John_token(client_John):
+    name = {"sub": client_John.user["name"], "scopes": ["user"]}
     token = create_access_token(name)
     return {"Authorization": f"Bearer {token}"}
 
 
 @pytest.fixture
-def token_admin(client):
-    name = {"sub": client.user["name"], "scopes": ["admin"]}
+def token_admin(client_John):
+    name = {"sub": client_John.user["name"], "scopes": ["admin"]}
     token = create_access_token(name)
     return {"Authorization": f"Bearer {token}"}
 
 
 @pytest.fixture
-def other_user(db: Session):
+def Jane(db: Session):
     user = UserIn(name="Jane Doe", email="john@gmail.com", password="123456")
     new_user = user.model_dump(exclude_unset=True)
     db.add(User(**new_user))
@@ -106,14 +108,154 @@ def other_user(db: Session):
 
 
 @pytest.fixture
-def other_client(other_user):
+def client_Jane(Jane):
     client = TestClient(app)
-    client.user = other_user
+    client.user = Jane
     return client
 
 
 @pytest.fixture
-def other_token_user(other_client):
-    name = {"sub": other_client.user["name"], "scopes": ["user"]}
+def Jane_token(client_Jane):
+    name = {"sub": client_Jane.user["name"], "scopes": ["user"]}
     token = create_access_token(name)
     return {"Authorization": f"Bearer {token}"}
+
+
+@pytest.fixture
+def account_John(client_John: TestClient, John_token: dict):
+    # Crear una cuenta de prueba para el usuario
+    response = client_John.post(
+        "/accounts/",
+        headers=John_token,
+        json={"name": "Cuenta Ahorros", "currency": "USD", "balance": 5000},
+    )
+    assert response.status_code == 201
+    return response.json()
+
+
+@pytest.fixture
+def global_expense_category(client_John: TestClient, token_admin: dict):
+    # Crear categorías de prueba para el administrador y el usuario
+    category = client_John.post(
+        "/categories/global",
+        headers=token_admin,
+        json={"name": "Categoría global", "type": TransactionType.EXPENSE.value},
+    )
+    return category.json()
+
+
+@pytest.fixture
+def John_income_category(client_John: TestClient, John_token: dict):
+    category = client_John.post(
+        "/categories/user",
+        headers=John_token,
+        json={
+            "name": "Categoría de usuario",
+            "type": TransactionType.INCOME.value,
+            "is_global": False,
+        },
+    )
+    return category.json()
+
+
+@pytest.fixture
+def expense_transaction_John(
+    client_John: TestClient,
+    John_token: dict,
+    account_John: dict,
+    global_expense_category: dict,
+):
+    transaction = client_John.post(
+        "/transactions",
+        headers=John_token,
+        json={
+            "category_id": global_expense_category["id"],
+            "account_id": account_John["id"],
+            "amount": 200.0,
+            "date": str(datetime.now()),
+            "comments": "Cobro de servicios",
+        },
+    )
+    assert transaction.status_code == 201
+    return transaction.json()
+
+
+@pytest.fixture
+def income_transaction_John(
+    client_John: TestClient,
+    John_token: dict,
+    account_John: dict,
+    John_income_category: dict,
+):
+    transaction = client_John.post(
+        "/transactions",
+        headers=John_token,
+        json={
+            "category_id": John_income_category["id"],
+            "account_id": account_John["id"],
+            "amount": 200.0,
+            "date": str(datetime.now()),
+            "comments": "Pago de servicios",
+        },
+    )
+    assert transaction.status_code == 201
+    return transaction.json()
+
+
+@pytest.fixture
+def budget_John(
+    client_John: TestClient,
+    John_token: dict,
+    global_expense_category: dict,
+):
+    start_date = datetime.now()
+    end_date = start_date + timedelta(days=30)
+    budget = client_John.post(
+        "/budgets/",
+        headers=John_token,
+        json={
+            "category_id": global_expense_category["id"],
+            "amount": 1000.0,
+            "period": {
+                "start_date": str(start_date),
+                "end_date": str(end_date),
+            },
+        },
+    )
+    assert budget.status_code == 201
+    return budget.json()
+
+
+@pytest.fixture
+def account_Jane(client_Jane: TestClient, Jane_token: dict):
+    # Crear una cuenta de prueba para el usuario
+    response = client_Jane.post(
+        "/accounts/",
+        headers=Jane_token,
+        json={"name": "Cuenta Ahorros", "currency": "USD", "balance": 5000},
+    )
+    assert response.status_code == 201
+    return response.json()
+
+
+@pytest.fixture
+def transaction_Jane(
+    client_Jane: TestClient,
+    Jane_token: dict,
+    account_Jane: int,
+    global_expense_category: int,
+):
+    date = str(datetime.now())
+    transaction = client_Jane.post(
+        "/transactions",
+        headers=Jane_token,
+        json={
+            "category_id": global_expense_category["id"],
+            "account_id": account_Jane["id"],
+            "amount": 200.0,
+            "date": date,
+            "comments": "Pago de servicios",
+        },
+    )
+    assert transaction.status_code == 201
+    return transaction.json()
