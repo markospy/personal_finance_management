80f1ec77f34e85cfb65aef40298f69859842a4e6
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-17 16:55:56 -0500

    "Move ProtectedRoutes component to utils/"

--- frontend/web/app/src/utils/ProtectedRoutes.tsx
+++ frontend/web/app/src/utils/ProtectedRoutes.tsx
@@ -3,9 +3,11 @@ import { useAuth } from "../context/AuthProvider";
 
 export const ProtectedRoutes = () => {
   const { isAuthenticated } = useAuth();
-
+    console.log('Entrando al protecto de ruta')
     if(!isAuthenticated) {
+      console.log('En el protecto de ruta: no logueado')
       return <Navigate to="/login" />;
     }
+    console.log('En el protecto de ruta: logueado')
     return <Outlet />;
   };
