c18bf0a49be77fff063d9e47b94522d4c032d838
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-17 16:56:18 -0500

    💣 Remove UserOut type and user state from AuthProvider, simplify login and logout functions.

--- frontend/web/app/src/context/AuthProvider.tsx
+++ frontend/web/app/src/context/AuthProvider.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useState, PropsWithChildren, useContext, useEffect } from 'react';
 import { getUser  } from "../api/user";
-import { UserOut } from '../schemas/user';
 
 // Crear el contexto
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -10,7 +9,6 @@ const AuthContext = createContext<any>(null);
 // Proveedor del contexto
 export const AuthProvider =  ({ children }: PropsWithChildren) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser ] = useState<UserOut | null>(null);
 
   const existingUser  = async () => {
       const token = localStorage.getItem('token');
@@ -24,27 +22,23 @@ export const AuthProvider =  ({ children }: PropsWithChildren) => {
   useEffect(() => {
       const checkExistingUser  = async () => {
           const userData = await existingUser ();
-          setUser(userData);
           setIsAuthenticated(!!userData);
       };
       checkExistingUser ();
   }, []);
 
-  const login = async (token: string) => {
+  const login = (token: string) => {
       localStorage.setItem('token', token);
-      const userData = await getUser (token);
       setIsAuthenticated(true);
-      setUser(userData);
   };
 
   const logout = () => {
       localStorage.removeItem('token');
       setIsAuthenticated(false);
-      setUser(null);
   };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
