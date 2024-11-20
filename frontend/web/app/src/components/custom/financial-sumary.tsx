071d86c372290ee01b005dc491008f2459091371
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-20 07:28:29 -0500

    🧩 "Refactored FinancialSummary component to accept a sumary object as a prop, updated component layout and content to display dynamic data"

--- frontend/web/app/src/components/custom/financial-sumary.tsx
+++ frontend/web/app/src/components/custom/financial-sumary.tsx
@@ -1,19 +1,33 @@
-export const FinancialSummary = () => (
+type Sumary = {
+  totalBalance: number;
+  totalIncomes: number;
+  totalExpenses: number
+};
+
+export function FinancialSummary({ sumary }) {
+  const currentMonth = new Date().toLocaleString('En', { month: 'long' });
+
+  return (
     <div className="bg-white p-6 rounded shadow-md mb-6">
-      <h2 className="text-xl font-bold mb-4">Financial Summary</h2>
-      <div className="grid grid-cols-3 gap-4">
+      <h2 className="text-xl font-bold mb-4">{currentMonth} Incomes and Expenses</h2>
+      <div className="grid grid-cols-4 gap-4">
         <div className="p-4 bg-gray-100 rounded">
-          <h3 className="text-lg font-semibold">Total Balance</h3>
-          <p className="text-2xl">$10,000</p>
+          <h3 className="text-lg font-semibold">Total Balance of Account</h3>
+          <p className="text-2xl">${sumary.total_balance}</p>
         </div>
         <div className="p-4 bg-gray-100 rounded">
           <h3 className="text-lg font-semibold">Total Income</h3>
-          <p className="text-2xl">$5,000</p>
+          <p className="text-2xl">${sumary.total_incomes}</p>
         </div>
         <div className="p-4 bg-gray-100 rounded">
           <h3 className="text-lg font-semibold">Total Expenses</h3>
-          <p className="text-2xl">$3,000</p>
+          <p className="text-2xl">${sumary.total_expenses}</p>
+        </div>
+        <div className="p-4 bg-gray-100 rounded">
+          <h3 className="text-lg font-semibold">Net balance of the month</h3>
+          <p className="text-2xl">${sumary.total_incomes - sumary.total_expenses}</p>
         </div>
       </div>
     </div>
   );
+};
