fd067b64327a2037173dbb1916b4148d7fbe18c2
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-21 15:22:36 -0500

    ➕ "Added getAccount function and import to account service"

--- frontend/web/app/src/services/account.ts
+++ frontend/web/app/src/services/account.ts
@@ -1,9 +1,17 @@
-import { getAccounts } from '@/api/account';
+import { getAccounts, getAccount } from '@/api/account';
 
 
 export function GetAccounts(token: string) {
     return {
         queryKey: ['account', 'all'],
         queryFn: () => getAccounts(token)
+    }
+}
+
+
+export function GetAccount(token: string, id: number) {
+    return {
+        queryKey: ['account', id],
+        queryFn: () => getAccount(token, id)
     }
 }
