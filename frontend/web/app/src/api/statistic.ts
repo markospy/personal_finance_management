8dde2f0f9098aedc378d1acc6ad6366d02607ccc
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-21 15:37:56 -0500

    🎁 "Added new types and API endpoints for monthly expenses and incomes, and updated service functions to use them."

--- frontend/web/app/src/api/statistic.ts
+++ frontend/web/app/src/api/statistic.ts
@@ -1,13 +1,85 @@
 import { axi } from "./config";
 import { DateIn } from "@/schemas/date";
 
-interface MonthlySumary {
-    total_expenses: number,
-    total_income: number
+type MonthlySumary = {
+    totalExpenses: number,
+    totalIncome: number
 }
+
+type MonthlyExpenses = {
+	categoryId: number,
+	categoryName: string,
+	totalAmount: number
+}
+
+type MonthlyIncomes = MonthlyExpenses;
 
 export const getMonthlySumary = (token: string, date:  DateIn): Promise<MonthlySumary> => {
     return axi.get('/statistics/monthly-summary',
+        {
+            params: {
+                year: date.year,
+                month: date.month
+            },
+            headers: {'Authorization': `Bearer ${token}`}
+        }
+    ).then(
+		response => {
+			console.log(response.status)
+			return response.data;
+		}
+	).catch(error => {
+		// Manejo de errores
+		if (error.response) {
+			console.log('Error en la respuesta del servidor:', error.response.data);
+			console.log('Estado del error:', error.response.status);
+			console.log('Encabezados del error:', error.response.headers);
+		} else if (error.request) {
+			console.log('No se recibió respuesta:', error.request);
+		} else {
+			console.log('Error al configurar la solicitud:', error.message);
+		}
+		console.log('Configuración de la solicitud:', error.config);
+
+		// Puedes lanzar el error de nuevo si quieres que la promesa se rechace
+		return Promise.reject(error);
+	});
+}
+
+export const getMonthlyExpenses = (token: string, date: DateIn): Promise<MonthlyExpenses[]> => {
+    return axi.get('/statistics/monthly-expenses',
+        {
+            params: {
+                year: date.year,
+                month: date.month
+            },
+            headers: {'Authorization': `Bearer ${token}`}
+        }
+    ).then(
+		response => {
+			console.log(response.status)
+			return response.data;
+		}
+	).catch(error => {
+		// Manejo de errores
+		if (error.response) {
+			console.log('Error en la respuesta del servidor:', error.response.data);
+			console.log('Estado del error:', error.response.status);
+			console.log('Encabezados del error:', error.response.headers);
+		} else if (error.request) {
+			console.log('No se recibió respuesta:', error.request);
+		} else {
+			console.log('Error al configurar la solicitud:', error.message);
+		}
+		console.log('Configuración de la solicitud:', error.config);
+
+		// Puedes lanzar el error de nuevo si quieres que la promesa se rechace
+		return Promise.reject(error);
+	});
+}
+
+export const getMonthlyIncomes = (token: string, date: DateIn): Promise<MonthlyIncomes[]> => {
+    return axi.get('/statistics/monthly-incomes',
         {
             params: {
                 year: date.year,
--- frontend/web/app/src/services/statistic.ts
+++ frontend/web/app/src/services/statistic.ts
@@ -3,7 +3,21 @@ import { DateIn } from "@/schemas/date";
 
 export function GetMonthlySumary(token: string, date: DateIn) {
     return {
-        queryKey: ["monthlySumary"],
+        queryKey: ["monthlySumary", date],
+        queryFn: () => getMonthlySumary(token, date),
+    }
+}
+
+export function GetMonthlyExpenses(token: string, date: DateIn) {
+    return {
+        queryKey: ["monthlySumary", date],
+        queryFn: () => getMonthlySumary(token, date),
+    }
+}
+
+export function GetMonthlyIncomes(token: string, date: DateIn) {
+    return {
+        queryKey: ["monthlySumary", date],
         queryFn: () => getMonthlySumary(token, date),
     }
 }
