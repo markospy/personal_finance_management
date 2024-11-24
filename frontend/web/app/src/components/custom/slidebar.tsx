6799fc0296efcdf70cde4fb02f8e20009e5c1971
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-24 00:37:48 -0500

    🧩 "Refactored Sidebar component to use NavLink, removed unnecessary imports and components from Dashboard.tsx, and replaced main content with Outlet component"

--- frontend/web/app/src/components/custom/slidebar.tsx
+++ frontend/web/app/src/components/custom/slidebar.tsx
@@ -1,11 +1,49 @@
+import { NavLink } from "react-router-dom";
+
+
 export const Sidebar = () => (
     <aside className="w-64 bg-white shadow-md">
       <ul>
-        <li className="p-4 border-b"><a href="#">Accounts</a></li>
-        <li className="p-4 border-b"><a href="#">Budget</a></li>
-        <li className="p-4 border-b"><a href="#">Expenses</a></li>
-        <li className="p-4 border-b"><a href="#">Income</a></li>
-        <li className="p-4 border-b"><a href="#">Categories</a></li>
+        <li className="p-4 border-b">
+          <NavLink
+            to="/dashboard/accounts"
+            className={({ isActive, isPending }) => isPending ? "bg-blue-200" : isActive ? "bg-blue-500" : ""}
+          >
+              Accounts
+          </NavLink>
+        </li>
+        <li className="p-4 border-b">
+          <NavLink
+            to="/dashboard/budget"
+            className={({ isActive, isPending }) => isPending ? "bg-blue-200" : isActive ? "bg-blue-500" : ""}
+          >
+              Budget
+          </NavLink>
+        </li>
+        <li className="p-4 border-b">
+          <NavLink
+            to="/dashboard/expenses"
+            className={({ isActive, isPending }) => isPending ? "bg-blue-200" : isActive ? "bg-blue-500" : ""}
+          >
+              Expenses
+          </NavLink>
+        </li>
+        <li className="p-4 border-b">
+          <NavLink
+            to="/dashboard/incomes"
+            className={({ isActive, isPending }) => isPending ? "bg-blue-200" : isActive ? "bg-blue-500" : ""}
+          >
+              Incomes
+          </NavLink>
+        </li>
+        <li className="p-4 border-b">
+          <NavLink
+            to="/dashboard/categories"
+            className={({ isActive, isPending }) => isPending ? "bg-blue-200" : isActive ? "bg-blue-500" : ""}
+          >
+              Categories
+          </NavLink>
+        </li>
       </ul>
     </aside>
   );
