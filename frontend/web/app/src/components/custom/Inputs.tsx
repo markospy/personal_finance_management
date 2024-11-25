d99861739ccd6c9c6f17f3afb9948d1b12146323
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-24 21:17:33 -0500

    🎨 Refactorize Input component and  add SelectScrollable component.

--- frontend/web/app/src/components/custom/Inputs.tsx
+++ frontend/web/app/src/components/custom/Inputs.tsx
@@ -1,56 +1,71 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 interface InputProps {
   title: string;
   name: string;
   type: string;
   required: boolean;
+  placeholder: string
 }
 
-interface InputPropsOptions extends InputProps {options?: any}
-
-
-export function Input({title, name, type, required}: InputProps) {
-    return (
-      <div className="mb-4">
-        <label className="block text-gray-700">{title}</label>
-        <input
-          name={name}
-          type={type}
-          className="w-full px-3 py-2 border rounded"
-          required={required}
-          min={0}
-        />
-      </div>
-    )
-  }
-
-
-export function InputObtions({title, name, type, required, options}: InputPropsOptions) {
-  return(
-    <div className="mb-4">
+export function Input({title, name, type, required, placeholder}: InputProps) {
+  return (
+    <div className="mb-2">
       <label className="block text-gray-700">{title}</label>
       <input
         name={name}
         type={type}
         className="w-full px-3 py-2 border rounded"
-        list="currencies"
         required={required}
+        min={0}
+        placeholder={placeholder}
       />
-      <datalist id="currencies">
-        {options.map((cur, index) => (
-            <option key={index} value={cur} />
-        ))}
-      </datalist>
     </div>
-  );
-};
+  )
+}
+
+export function SelectScrollable({title, name, options, description}: {title: string, name: string, options: Record<string, string[]>, description: string}) {
+
+
+  return (
+    <Select name={name}>
+      <label className="block text-gray-700">{title}</label>
+      <SelectTrigger className="w-full mb-2 h-10">
+        <SelectValue placeholder={description} />
+      </SelectTrigger>
+      <SelectContent>
+        {options &&
+          (
+            Object.keys(options).map(option => (
+              <SelectGroup key={option}>
+                <SelectLabel >{option.toUpperCase()}</SelectLabel>
+                {options[option].map(value => (
+                  <SelectItem value={value}>{value}</SelectItem>
+                ))}
+              </SelectGroup>
+            ))
+          )
+        }
+      </SelectContent>
+    </Select>
+  )
+}
 
 export function InputTextTarea({title, name}:{title:string, name: string}) {
   return (
-    <div className="mb-4">
-      <label className="block mb-2">{title}</label>
+    <div className="mb-2">
+      <label className="block">{title}</label>
       <textarea
         name={name}
+        placeholder="Add a comment"
         className="w-full p-2 border rounded"
       ></textarea>
     </div>
@@ -63,8 +78,8 @@ export function InputDate({title, name}:{title:string, name: string}) {
   const mm = String(today.getMonth() + 1).padStart(2, '0');
   const dd = String(today.getDate()).padStart(2, '0');
   return (
-    <div className="mb-4">
-      <label className="block mb-2">{title}</label>
+    <div className="mb-2">
+      <label className="block">{title}</label>
       <input
         type="date"
         name={name}
