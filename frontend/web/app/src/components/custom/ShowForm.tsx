1da9c41a5a725008f8004c80c053409f85c3074c
Marcos Avila <marcos.seocuba@gmail.com>
2024-11-27 13:02:36 -0500

    🧩 "Added active and focus styles to ButtonShowForm component"

--- frontend/web/app/src/components/custom/ShowForm.tsx
+++ frontend/web/app/src/components/custom/ShowForm.tsx
@@ -6,7 +6,7 @@ interface Props {
 export function ButtonShowForm({title, onClick}:Props) {
     return (
       <button
-      className="bg-blue-500 text-white px-4 py-2 rounded mb-4 shadow-md font-medium hover:shadow-xl hover:bg-white hover:text-blue-500 hover:font-bold hover:outline"
+      className="bg-blue-500 text-white px-4 py-2 rounded mb-4 shadow-md font-medium hover:shadow-xl hover:bg-white hover:text-blue-500 hover:font-bold hover:outline active:bg-blue-100 focus:shadow-xl focus:bg-white focus:text-blue-500 focus:font-bold focus:outline"
       onClick={onClick}
       >
       {title}
