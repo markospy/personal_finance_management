a9a4a8fd38aa082056c4ed63974ef9a0cfdb1a45
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-02 17:04:11 -0500

    🔨 "Refactor getUser function to return ErrorResponse and handle errors differently"

--- frontend/web/app/src/api/user.ts
+++ frontend/web/app/src/api/user.ts
@@ -20,27 +20,21 @@ export const createUser = (user: UserIn): Promise<UserOut | ErrorResponse > => {
 };
 
 
-export const getUser = (token: string): Promise<UserOut> => {
-
-    return axi.get('/user/me', {headers: {'Authorization': `Bearer ${token}`}})
+export const getUser  = (token: string): Promise<UserOut | ErrorResponse> => {
+    return axi.get('/user/me', { headers: { 'Authorization': `Bearer ${token}` } })
         .then(response => {
-            console.log(response.status)
+            console.log(response.status);
             return response.data; // Devuelve los datos de la respuesta
         })
-        .catch(error => {
+        .catch((error: AxiosError) => {
             // Manejo de errores
-            if (error.response) {
-                console.log('Error en la respuesta del servidor:', error.response.data);
-                console.log('Estado del error:', error.response.status);
-                console.log('Encabezados del error:', error.response.headers);
-            } else if (error.request) {
-                console.log('No se recibió respuesta:', error.request);
-            } else {
-                console.log('Error al configurar la solicitud:', error.message);
-            }
-            console.log('Configuración de la solicitud:', error.config);
+            const status = error.response?.status || 500; // Valor por defecto
+            const msg = error.message;
 
-            // Puedes lanzar el error de nuevo si quieres que la promesa se rechace
-            return Promise.reject(error);
+            // Retornar un objeto de error
+            return {
+                status,
+                msg
+            };
         });
 };
