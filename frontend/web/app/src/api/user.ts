029c4704e45727c217804987f627ff16df2165cd
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-02 17:00:38 -0500

    🤸‍♀ "Updated createUser function in user.ts to return ErrorResponse instead of Error, and refactored error handling to return a custom error object."

--- frontend/web/app/src/api/user.ts
+++ frontend/web/app/src/api/user.ts
@@ -1,24 +1,21 @@
 import { UserIn, UserOut } from "../schemas/user";
+import { ErrorResponse  } from "@/schemas/error";
+import { AxiosError } from 'axios';
 import { axi } from "./axiosConfig";
 
-export const createUser = (user: UserIn): Promise<UserOut | Error> => {
+
+export const createUser = (user: UserIn): Promise<UserOut | ErrorResponse > => {
     return axi.post('/user', user)
         .then(response => response.data)
-        .catch(error => {
-            // Manejo de errores
-            if (error.response) {
-                console.log('Error en la respuesta del servidor:', error.response.data);
-                console.log('Estado del error:', error.response.status);
-                console.log('Encabezados del error:', error.response.headers);
-            } else if (error.request) {
-                console.log('No se recibió respuesta:', error.request);
-            } else {
-                console.log('Error al configurar la solicitud:', error.message);
-            }
-            console.log('Configuración de la solicitud:', error.config);
+        .catch((error: AxiosError) => {
+            // Asegúrate de que error.response esté definido
+            const status = error.response?.status || 500; // Valor por defecto
+            const msg = error.message;
 
-            // Puedes lanzar el error de nuevo si quieres que la promesa se rechace
-            return Promise.reject(error);
+            return {
+                status,
+                msg
+            };
         });
 };
 
