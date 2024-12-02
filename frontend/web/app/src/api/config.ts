88923f61768a95731cb4402bee7f9a6d3e183e2d
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-02 01:44:40 -0500

    "Update API config to use environment variable for URL"

--- frontend/web/app/src/api/config.ts
+++ frontend/web/app/src/api/config.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8000';
 
 export const axi = axios.create({
-	baseURL: API_URL,
+	baseURL: process.env.API_URL,
 	timeout: 1000,
 	headers: {'Accept': 'application/json'},
 });
