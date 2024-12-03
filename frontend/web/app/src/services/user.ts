e7044d40a601a113044e2b67f26a28f29a84a5cd
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-02 23:20:10 -0500

    💡 "Wrap GetUser return value with queryOptions"

--- frontend/web/app/src/services/user.ts
+++ frontend/web/app/src/services/user.ts
@@ -1,11 +1,12 @@
+import { queryOptions } from "@tanstack/react-query";
 import { getUser } from "../api/user";
 
 
 export function GetUser(token: string) {
-    return {
+    return queryOptions({
         queryKey: ['user', 'me'],
         queryFn: () => getUser(token),
         staleTime: Infinity,
         gcTime: Infinity,
-    }
+    });
 }
