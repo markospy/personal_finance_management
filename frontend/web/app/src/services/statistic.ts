4966c0a3614f8fa943c920377772518433b1f800
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-02 23:22:54 -0500

    ✈️ "Removed imports and functions, replaced object literals with queryOptions calls"

--- frontend/web/app/src/services/statistic.ts
+++ frontend/web/app/src/services/statistic.ts
@@ -1,58 +1,24 @@
-import { getMonthlyExpenses, getMonthlyIncomes, getMonthlySumary, MonthlyExpenses, MonthlyIncomes, MonthlySumary } from "@/api/statistic";
+import { getMonthlyExpenses, getMonthlyIncomes, getMonthlySumary } from "@/api/statistic";
 import { DateIn } from "@/schemas/date";
-import { QueryClient } from "@tanstack/react-query";
+import {  queryOptions } from "@tanstack/react-query";
 
 export function GetMonthlySumary(token: string, date: DateIn) {
-    return {
+    return queryOptions({
         queryKey: ["monthlySumary", date],
         queryFn: () => getMonthlySumary(token, date),
-    }
+    })
 }
 
 export function GetMonthlyExpenses(token: string, date: DateIn) {
-    return {
+    return queryOptions({
         queryKey: ["monthlyExpenses", date],
         queryFn: () => getMonthlyExpenses(token, date),
-    }
+    })
 }
 
 export function GetMonthlyIncomes(token: string, date: DateIn) {
-    return {
+    return queryOptions({
         queryKey: ["monthlyIncomes", date],
         queryFn: () => getMonthlyIncomes(token, date),
-    }
-}
-
-export async function GetMonthlySumaryTryCatch(token: string, date: DateIn, queryClient: QueryClient) {
-    let summary: MonthlySumary | null = null;
-    try {
-        summary = await queryClient.ensureQueryData(GetMonthlySumary(token, date));
-    } catch (error) {
-    console.error('Error fetching monthly summary:', error);
-    // summary seguirá siendo null si hay un error
-    }
-    return summary;
-}
-
-export async function GetMonthlyExpensesTryCatch(token: string, date: DateIn, queryClient: QueryClient) {
-    let summaryExpenses: MonthlyExpenses[] | null = null;
-    try {
-        summaryExpenses = await queryClient.ensureQueryData(GetMonthlyExpenses(token, date));
-    } catch (error) {
-    console.error('Error fetching monthly expenses:', error);
-    // summary seguirá siendo null si hay un error
-    }
-    return summaryExpenses;
-}
-
-
-export async function GetMonthlyIncomesTryCatch(token: string, date: DateIn, queryClient: QueryClient) {
-    let summaryIncomes: MonthlyIncomes[] | null = null;
-    try {
-        summaryIncomes = await queryClient.ensureQueryData(GetMonthlyIncomes(token, date));
-    } catch (error) {
-    console.error('Error fetching monthly incomes:', error);
-    // summary seguirá siendo null si hay un error
-    }
-    return summaryIncomes;
+    })
 }
