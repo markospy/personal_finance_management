76d885623558c741b3dfecc6dc9b8ba9eab19def
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-02 23:26:33 -0500

    🔌 "Refactor account service: simplify GetAccounts and GetAccount functions, remove GetAccountsTryCatch function and AccountOut import"

--- frontend/web/app/src/services/account.ts
+++ frontend/web/app/src/services/account.ts
@@ -1,30 +1,18 @@
 import { getAccounts, getAccount } from '@/api/account';
-import { AccountOut } from '@/schemas/account';
-import { QueryClient } from '@tanstack/react-query';
+import { queryOptions } from '@tanstack/react-query';
 
 
 export function GetAccounts(token: string) {
-    return {
+    return queryOptions({
         queryKey: ['account', 'all'],
         queryFn: () => getAccounts(token)
-    }
+    });
 }
 
 
 export function GetAccount(token: string, id: number) {
-    return {
+    return queryOptions({
         queryKey: ['account', id],
         queryFn: () => getAccount(token, id)
-    }
-}
-
-
-export async function GetAccountsTryCatch(token: string, queryClient: QueryClient) {
-    let accounts: AccountOut[] | null = null;
-    try {
-        accounts = await queryClient.ensureQueryData(GetAccounts(token));
-    } catch (error) {
-        console.error('Error fetching accounts:', error);
-    }
-    return accounts;
+    });
 }
