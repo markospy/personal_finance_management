c8ad0d1b3fcea9606c3c0047ea3e7f03b3b70ee4
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-02 23:11:25 -0500

    📱 "Updated .gitignore to ignore backend/app/my_database.db-journal file and refactored frontend/web/app/src/api/auth.ts code with minor formatting changes."

--- frontend/web/app/src/api/auth.ts
+++ frontend/web/app/src/api/auth.ts
@@ -3,32 +3,32 @@ import { ErrorResponse  } from "@/schemas/error";
 import { AxiosError } from 'axios';
 
 export type TokenOut = {
-    access_token: string;
-    token_type: string
+  access_token: string;
+  token_type: string
 }
 
 axi.defaults.timeout = 5000
 
 export const getToken = (username: string, password: string): Promise<TokenOut | ErrorResponse> => {
-    return axi.post(
-        '/token',
-        `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}&scope=user`,
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        }
-    ).then(response => {
-        return response.data; // Devuelve los datos de la respuesta
-    }).catch((error: AxiosError) => {
-        const status = error.response?.status || 500; // Valor por defecto
-        const msg = error.message;
+  return axi.post(
+    '/token',
+    `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}&scope=user`,
+    {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }
+  ).then(response => {
+    return response.data; // Devuelve los datos de la respuesta
+  }).catch((error: AxiosError) => {
+    const status = error.response?.status || 500; // Valor por defecto
+    const msg = error.message;
 
-        // Retornar un objeto de error
-        return {
-            status,
-            msg
-        };
-    });
+    // Retornar un objeto de error
+    return {
+      status,
+      msg
+    };
+  });
 };
