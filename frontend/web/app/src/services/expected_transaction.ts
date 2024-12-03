8fee74ec09300ec6a2bae1dd998bcc06801d7fa0
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-02 23:23:50 -0500

    📦 "Wrap query objects with queryOptions in expected_transaction service"

--- frontend/web/app/src/services/expected_transaction.ts
+++ frontend/web/app/src/services/expected_transaction.ts
@@ -1,15 +1,16 @@
 import { getExpTransactions, getExpTransaction } from '@/api/expected_transaction';
+import { queryOptions } from '@tanstack/react-query';
 
 export function GetExpTransactions(token: string) {
-    return {
+    return queryOptions({
         queryKey: ['expected transaction'],
         queryFn: () => getExpTransactions(token)
-    }
+    });
 }
 
 export function GetExpTransaction(token: string, id: number) {
-    return {
+    return queryOptions({
         queryKey: ['expected transaction', id],
         queryFn: () => getExpTransaction(token, id)
-    }
+    });
 }
