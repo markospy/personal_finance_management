932e1f7e499d3bc05a297d8860e16fae9237d504
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-05 01:15:05 -0500

    🔐 "Updated guard functions in guards.ts to include type checks for MonthlySummary, MonthlyIncomes, MonthlyExpenses, AccountOut, and added new guard function for CategoryOut."

--- frontend/web/app/src/utils/guards.ts
+++ frontend/web/app/src/utils/guards.ts
@@ -1,17 +1,18 @@
 import { MonthlyExpenses, MonthlyIncomes, MonthlySumary } from "@/api/statistic";
 import { AccountOut } from "@/schemas/account";
+import { CategoryOut } from "@/schemas/category";
 import { ErrorResponse } from "@/schemas/error";
 
 export const isMonthlySummary = (summary: MonthlySumary | ErrorResponse | false): summary is MonthlySumary => {
-return (
+  return (
     typeof summary === 'object' &&
     'totalExpenses' in summary && typeof summary.totalExpenses === 'number' &&
     'totalIncomes' in summary && typeof summary.totalIncomes === 'number'
-);
+  );
 };
 
 export const isMonthlyIncomes = (incomes: MonthlyIncomes[] | ErrorResponse | false): incomes is MonthlyIncomes[] => {
-return (
+  return (
     Array.isArray(incomes) &&
     incomes.every(income =>
     typeof income === 'object' &&
@@ -19,11 +20,11 @@ return (
     'categoryName' in income && typeof income.categoryName === 'string' &&
     'totalAmount' in income && typeof income.totalAmount === 'number'
     )
-);
+  );
 };
 
 export const isMonthlyExpenses = (expenses: MonthlyExpenses[] | ErrorResponse | false): expenses is MonthlyExpenses[] => {
-return (
+  return (
     Array.isArray(expenses) &&
     expenses.every(expense =>
     typeof expense === 'object' &&
@@ -31,17 +32,33 @@ return (
     'categoryName' in expense && typeof expense.categoryName === 'string' &&
     'totalAmount' in expense && typeof expense.totalAmount === 'number'
     )
-);
+  );
 };
 
 export const isAccount = (accounts: AccountOut[] | ErrorResponse): accounts is AccountOut[] => {
-return (
-    typeof accounts === 'object' &&
-    'accounts' in accounts &&
-    'currency' in accounts && (typeof accounts.currency === 'string') &&
-    'balance' in accounts && (typeof accounts.balance === 'number') &&
-    'name' in accounts && (typeof accounts.name === 'string') &&
-    'id' in accounts && (typeof accounts.id === 'number') &&
-    'userId' in accounts && (typeof accounts.userId === 'number')
-);
+  return (
+    Array.isArray(accounts) &&
+    accounts.every(account =>
+    typeof account === 'object' &&
+    'currency' in account && (typeof account.currency === 'string') &&
+    'balance' in account && (typeof account.balance === 'number') &&
+    'name' in account && (typeof account.name === 'string') &&
+    'id' in account && (typeof account.id === 'number') &&
+    'userId' in account && (typeof account.userId === 'number')
+    )
+  );
+};
+
+export const isCategory = (categories: CategoryOut[] | ErrorResponse): categories is CategoryOut[] => {
+  return (
+    Array.isArray(categories) &&
+    categories.every(category =>
+    typeof category === 'object' &&
+    'type' in category && (typeof category.type === 'string') &&
+    'id' in category && (typeof category.id === 'number') &&
+    'name' in category && (typeof category.name === 'string') &&
+    'isGlobal' in category && (typeof category.isGlobal === 'boolean') &&
+    'userId' in category && (typeof category.userId === 'number' || category.userId === null)
+    )
+  );
 };
