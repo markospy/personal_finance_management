e5d6fe0a38f1edf97d8f20e6411d08f1d1828835
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-08 22:41:41 -0500

    🛠 Add changes in interface where change fetcher.state

--- frontend/web/app/src/components/custom/WrapperForms.tsx
+++ frontend/web/app/src/components/custom/WrapperForms.tsx
@@ -5,6 +5,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import './Spinner.css';
 
 interface Props {
   title: string;
@@ -15,22 +16,21 @@ interface Props {
 
 export function WrapperForms({title, url, children, onClick}:Props ) {
   const fetcher = useFetcher();
-
-  const handleSubmitClick = () => {
-    // Llama a la función onClick
-    if (onClick) {
-      onClick();
-    }
-    // No llamamos a event.preventDefault() para permitir que el formulario se envíe
-  };
+  const busy = fetcher.state !== 'idle';
+  // useEffect(() => {
+  //   if (busy) {
+  //     onClick(); // Llama a la función que cierra el modal
+  //   }
+  // }, [busy, onClick]);
 
   return (
     <Card className="fixed z-50 inset-y-20 inset-x-1/3 bg-white rounded-lg shadow-lg p-2 max-w-md max-h-fit">
+      {fetcher.state!=='idle' && <div className="absolute inset-0 bg-white opacity-50"></div>}
       <CardHeader>
         <CardTitle className="text-2xl font-bold text-blue-900">{title}</CardTitle>
       </CardHeader>
       <CardContent>
-        <fetcher.Form method="post" action={url}>
+        <fetcher.Form method="post" action={url} >
           <>
             {children}
           </>
@@ -39,14 +39,17 @@ export function WrapperForms({title, url, children, onClick}:Props ) {
               type="button"
               className="bg-gray-500 text-white px-4 py-2 rounded mr-2"
               onClick={onClick}
+              disabled={fetcher.state!=='idle'}
             >
               Cancel
             </button>
             <button
-                type="submit"
-                className="bg-blue-500 text-white py-2 px-4 rounded"
-              >
-                Create
+              type="submit"
+              className="flex gap-2 items-center bg-blue-500 text-white py-2 px-4 rounded"
+              disabled={fetcher.state!=='idle'}
+            >
+              {fetcher.state!=='idle' && <div className="spinner"/>}
+              Create
             </button>
           </div>
         </fetcher.Form>
