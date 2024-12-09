161d8aad866a89b0ce3b41c6f4263b3289a94bc9
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-09 01:08:47 -0500

    ➕ Add GlobalSpinner component

---
+++ frontend/web/app/src/components/custom/GlobalSpinner.tsx
@@ -0,0 +1,8 @@
+import './GlobalSpinner.css';
+
+export default function GlobalSpinner() {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+        <div className='globalspinner'></div>
+    </div>
+)};
