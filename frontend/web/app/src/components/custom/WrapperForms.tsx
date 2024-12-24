1ca9f9fdd7c0fed8ace7b6123aa4eccd2a7fdc6b
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-24 04:18:22 -0500

    🔨 `Refactor WrapperForms component to use react-query mutation and data provider`

--- frontend/web/app/src/components/custom/WrapperForms.tsx
+++ frontend/web/app/src/components/custom/WrapperForms.tsx
@@ -1,4 +1,3 @@
-import { useFetcher } from "react-router-dom";
 import {
   Card,
   CardContent,
@@ -6,30 +5,41 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import './Spinner.css';
+import { QueryClient, UseMutationResult } from "@tanstack/react-query";
+import { NewTransaction, NewTransactionProps } from "./TransactionModal";
+import { getToken } from "@/utils/token";
 
 interface Props {
   title: string;
-  url: string;
+  mutation: UseMutationResult<NewTransaction, Error, NewTransaction, unknown>;
+  dataProvider: ({queryClient, token, data}: NewTransactionProps) => Promise<NewTransaction>;
   children: React.ReactNode;
-  onClick: () => void
+  onClick: () => void;
+  queryClient: QueryClient;
 }
 
-export function WrapperForms({title, url, children, onClick}:Props ) {
-  const fetcher = useFetcher();
+export function WrapperForms({title, mutation, dataProvider, children, onClick, queryClient}:Props ) {
 
   return (
     <Card className="fixed z-50 inset-y-20 inset-x-1/3 bg-white rounded-lg shadow-lg p-2 max-w-md max-h-fit">
-      {fetcher.state!=='idle' && <div className="absolute inset-0 bg-white opacity-50"></div>}
-      {fetcher.data?.error && (
-        <p style={{ color: "red" }}>
-          {fetcher.data.error}
-        </p>
-      )}
       <CardHeader>
         <CardTitle className="text-2xl font-bold text-blue-900">{title}</CardTitle>
       </CardHeader>
       <CardContent>
-        <fetcher.Form method="post" action={url} >
+        <form onSubmit={async (e) => {
+            e.preventDefault();
+            const token = getToken();
+            // Accede a los datos del formulario
+            const formData = new FormData(e.target);
+
+            // Aquí puedes convertir formData a un objeto si es necesario
+            const data = Object.fromEntries(formData.entries());
+            console.log(data);
+
+            const mutationData = await dataProvider({queryClient, token, data});
+            console.log(mutationData)
+            mutation.mutate(mutationData)
+          }} >
           <>
             {children}
           </>
@@ -38,20 +48,20 @@ export function WrapperForms({title, url, children, onClick}:Props ) {
               type="button"
               className="bg-gray-500 text-white px-4 py-2 rounded mr-2"
               onClick={onClick}
-              disabled={fetcher.state!=='idle'}
+              disabled={mutation.isPending}
             >
               Cancel
             </button>
             <button
               type="submit"
               className="flex gap-2 items-center bg-blue-500 text-white py-2 px-4 rounded"
-              disabled={fetcher.state!=='idle'}
+              disabled={mutation.isPending}
             >
-              {fetcher.state!=='idle' && <div className="spinner"/>}
+              {mutation.isPending && <div className="spinner"/>}
               Create
             </button>
           </div>
-        </fetcher.Form>
+        </form>
       </CardContent>
     </Card>
   );
