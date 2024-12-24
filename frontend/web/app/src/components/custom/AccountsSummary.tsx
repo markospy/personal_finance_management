b8722ff0259e2daf06b072c60f1178291212766a
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-24 04:17:47 -0500

    ⚙️ `Added queryClient parameter to AccountsSummary component and TransactionModal component`

--- frontend/web/app/src/components/custom/AccountsSummary.tsx
+++ frontend/web/app/src/components/custom/AccountsSummary.tsx
@@ -1,10 +1,11 @@
 import { AccountsCategories } from "@/routes/Report";
 import { Card, CardContent } from "../ui/card";
 import { TransactionModal } from "./TransactionModal";
+import { QueryClient } from "@tanstack/react-query";
 
 
 
-export default function AccountsSummary({ data }:{data: AccountsCategories}) {
+export default function AccountsSummary({ data, queryClient }:{data: AccountsCategories, queryClient: QueryClient}) {
   let balance: number = 0;
   if(data.accounts) {
     data.accounts.forEach((account) => balance+=account.balance)
@@ -17,7 +18,7 @@ export default function AccountsSummary({ data }:{data: AccountsCategories}) {
         <p className="text-sm font-medium text-gray-500">Saldo actual</p>
         <h2 className="text-3xl font-bold">${balance}</h2>
       </div>
-      <TransactionModal data={data} />
+      <TransactionModal data={data} queryClient={queryClient} />
       </CardContent>
     </Card>
   );
