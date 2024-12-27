2ad00263861660f89f13b43558e6452d9ebc72ca
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2024-12-27 14:25:58 -0500

    🗑 `feat: Add confirmation dialog for account deletion`

--- frontend/web/app/src/routes/UserAccounts.tsx
+++ frontend/web/app/src/routes/UserAccounts.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { ArrowBigRightDash, PencilLine, Plus, Trash2 } from 'lucide-react'
+import { ArrowBigRightDash, PencilLine, Plus, Trash2, TriangleAlert } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import currencies from '@/utils/currencies'
 import { AccountOut } from '@/schemas/account'
@@ -10,6 +10,7 @@ import { ErrorResponse } from '@/schemas/error'
 import { QueryClient } from '@tanstack/react-query'
 import { deleteAccount } from '@/api/account'
 import { useToast } from "@/hooks/use-toast"
+import { useState } from 'react'
 
 export const loader = (queryClient: QueryClient) => async () => {
   const token = getToken();
@@ -25,6 +26,8 @@ const getCurrencyIcon = (currency: string) => {
 }
 
 export default function UserAccounts() {
+  const [ showConfirm, setShowConfirm ] = useState(false);
+  const [ accountToDelete, setAccountToDelete ] = useState({id:-1, name:''});
   const accounts: AccountOut[] = useLoaderData() as AccountOut[];
   const { toast } = useToast()
   const token = getToken();
@@ -54,7 +57,7 @@ export default function UserAccounts() {
             <CardHeader className="flex flex-row justify-between items-center space-y-0 pb-2">
               <CardTitle className="flex items-center gap-8 font-medium text-sm">
                 {account.name}
-                <PencilLine className='hover:text-blue-500 cursor-pointer size-5'/>
+                <PencilLine className={`hover:text-blue-500 cursor-pointer size-5 ${showConfirm && 'cursor-not-allowed hover:text-black'}`}/>
               </CardTitle>
               <p>{getCurrencyIcon(account.currency) || account.currency}</p>
             </CardHeader>
@@ -70,7 +73,14 @@ export default function UserAccounts() {
                   <p className="text-muted-foreground text-xs">
                     Current balance
                   </p>
-                  <Trash2 onClick={async() => await delAccount(token, account.id)} className='text-red-600 hover:cursor-pointer size-4'/>
+                  <Trash2 onClick={() => {
+                      if(!showConfirm){
+                        setShowConfirm(true);
+                        setAccountToDelete({id: account.id, name: account.name});
+                      }
+                    }}
+                    className={`text-red-600 cursor-pointer size-4 ${showConfirm && 'cursor-not-allowed'}`}
+                  />
                 </div>
               </div>
             </CardContent>
@@ -81,6 +91,20 @@ export default function UserAccounts() {
               <Plus className='text-gray-300 size-16'/>
             </CardContent>
           </Card>
+          {showConfirm && (
+            <div className='top-0 left-0 fixed flex justify-center items-center w-full min-h-screen' >
+              <div className='space-y-6 bg-red-100 p-6 rounded-md animate-slide-in-bottom align-middle'>
+                <div className='flex gap-2'>
+                  <TriangleAlert className='flex-none text-red-500'/>
+                  <p className='font-medium text-red-500'>Do you confirm that you want to delete the account {accountToDelete.name}?</p>
+                </div>
+                <div className='flex justify-center gap-4'>
+                  <button className='bg-gray-400 hover:bg-gray-600 p-2 rounded-md text-base text-white' onClick={() => setShowConfirm(false)}>Cancel</button>
+                  <button className="bg-red-400 hover:bg-red-600 p-2 rounded-md text-base text-white" onClick={async() => await delAccount(token, accountToDelete.id)}>Delete</button>
+                </div>
+              </div>
+            </div>
+          )}
       </div>
     </div>
   )
