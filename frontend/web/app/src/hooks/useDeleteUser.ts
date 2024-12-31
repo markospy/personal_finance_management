6d1243dc304cae5a5c9a0bf6616dec4b57f8ffbb
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2024-12-30 19:30:47 -0500

    🔥 `Removed QueryClient dependency from useDeleteUser and useNewTransaction hooks`

--- frontend/web/app/src/hooks/useDeleteUser.ts
+++ frontend/web/app/src/hooks/useDeleteUser.ts
@@ -1,17 +1,15 @@
 import { deleteUser  } from "@/api/user";
-import { QueryClient, useMutation } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useToast } from "./use-toast";
 import { useAuth } from "@/context/AuthProvider";
 
-export const useDeleteUser  = (queryClient: QueryClient) => {
+export const useDeleteUser  = () => {
   const { toast } = useToast();
   const { logout} = useAuth();
 
   const mutation = useMutation<string, Error, { token: string }>({
     mutationFn: ({ token }) => deleteUser (token),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['user', 'all'] });
-      queryClient.removeQueries()
       logout()
       toast({ title: "Success", description: "User  deleted successfully", className: "bg-green-200" });
     },
