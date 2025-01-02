f229b0fde7ac459d019e688be2bcf7e3e15c33fd
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2025-01-02 17:18:31 -0500

    🔧 `Refactor AccountsSummary component to display balance by currency`

--- frontend/web/app/src/components/custom/AccountsSummary.tsx
+++ frontend/web/app/src/components/custom/AccountsSummary.tsx
@@ -3,19 +3,37 @@ import { Card, CardContent } from "../ui/card";
 import { TransactionModal } from "./TransactionModal";
 import { QueryClient } from "@tanstack/react-query";
 import { AccountForm, newAccount } from "./AccountModal";
-import { CirclePlus, CreditCard } from "lucide-react";
+import { CirclePlus, CreditCard, Currency } from "lucide-react";
 import { useState } from "react";
 import Tippy from '@tippyjs/react'; // Asegúrate de que Tippy.js esté instalado
 import { Link } from "react-router-dom";
 import { useNewAccount } from "@/hooks/useNewAccount";
 
-export default function AccountsSummary({ data, queryClient }: { data: AccountsCategories, queryClient: QueryClient }) {
+interface Props {
+  data: AccountsCategories;
+  queryClient: QueryClient;
+}
+
+interface Currency {
+  currency: string;
+  total: number;
+}
+
+export default function AccountsSummary({ data, queryClient }:Props ) {
   const [lookForm, setLookForm] = useState(false);
   const addAccountMutation = useNewAccount(queryClient);
-  let balance: number = 0;
+
+  const currencies: Currency[] = [];
 
   if (data.accounts) {
-    data.accounts.forEach((account) => balance += account.balance);
+    data.accounts.forEach((account) => {
+      const currency = currencies.find(item => account.currency === item.currency);
+      if (currency) {
+        currency.total += account.balance;
+      } else {
+        currencies.push({ currency: account.currency, total: account.balance });
+      }
+    });
   }
 
   return (
@@ -43,9 +61,16 @@ export default function AccountsSummary({ data, queryClient }: { data: AccountsC
           mutation={addAccountMutation}
           dataProvider={newAccount}
         />}
-        <div className="text-center animate-blurred-fade-in">
-          <p className="w-full font-medium text-base text-center text-gray-500">Current Balance</p>
-          <h2 className="font-bold text-3xl">${balance}</h2>
+        <div className="space-y-8 text-center animate-blurred-fade-in">
+          <p className="w-full font-medium text-base text-center text-gray-500">Current Balance by Currency</p>
+          <div className="flex justify-around">
+            {currencies.map(currency => (
+              <div key={currency.currency} className="flex items-baseline gap-2 mb-8">
+                <h2 className="font-bold text-3xl">${currency.total}</h2>
+                <h3 className="font-medium text-base">{currency.currency}</h3>
+              </div>
+            ))}
+          </div>
         </div>
         <TransactionModal data={data} queryClient={queryClient} />
       </CardContent>
