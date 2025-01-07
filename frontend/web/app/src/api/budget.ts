f5742ea7827829db9828d15bfca199e9e4e6cc93
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2025-01-07 11:03:42 -0500

    `Updated budget API and services with minor formatting changes`

--- frontend/web/app/src/api/budget.ts
+++ frontend/web/app/src/api/budget.ts
@@ -6,115 +6,115 @@ import { BudgetIn, BudgetOut, BudgetStatus, BudgetUpdate } from "../schemas/budg
 
 export const createBudget = (token: string, budget: BudgetIn): Promise<BudgetOut | ErrorResponse> => {
 	return axi.post('/budgets/', {
-			"category_id": budget.category_id,
-			"amount": budget.amount,
-			"period": {
-					"start_date": budget.period.start_date,
-					"end_date": budget.period.end_date
-			}
+		"category_id": budget.category_id,
+		"amount": budget.amount,
+		"period": {
+			"start_date": budget.period.start_date,
+			"end_date": budget.period.end_date
+		}
 	}, { headers: { 'Authorization': `Bearer ${token}` } })
 	.then(response => {
-			console.log(response.status);
-			return response.data; // Devuelve los datos de la respuesta
+		console.log(response.status);
+		return response.data; // Devuelve los datos de la respuesta
 	})
 	.catch((error: AxiosError) => {
-			const status = error.response?.status || 500; // Valor por defecto
-			const msg = error.message;
+		const status = error.response?.status || 500; // Valor por defecto
+		const msg = error.message;
 
-			// Retornar un objeto de error
-			return {
-					status,
-					msg
-			};
+		// Retornar un objeto de error
+		return {
+			status,
+			msg
+		};
 	});
 };
 
 export const getBudgets = (token: string): Promise<BudgetOut[] | ErrorResponse> => {
 	return axi.get('/budgets/', { headers: { 'Authorization': `Bearer ${token}` } })
 	.then(response => {
-			console.log(response.status);
-			return response.data; // Devuelve los datos de la respuesta
+		console.log(response.status);
+		return response.data; // Devuelve los datos de la respuesta
 	})
 	.catch((error: AxiosError) => {
-			const status = error.response?.status || 500; // Valor por defecto
-			const msg = error.message;
+		const status = error.response?.status || 500; // Valor por defecto
+		const msg = error.message;
 
-			// Retornar un objeto de error
-			return {
-					status,
-					msg
-			};
+		// Retornar un objeto de error
+		return {
+			status,
+			msg
+		};
 	});
 };
 
 export const getBudget = (token: string, id: number): Promise<BudgetOut | ErrorResponse> => {
 	return axi.get(`/budgets/${id}`, { headers: { 'Authorization': `Bearer ${token}` } })
 	.then(response => {
-			console.log(response.status);
-			return response.data; // Devuelve los datos de la respuesta
+		console.log(response.status);
+		return response.data; // Devuelve los datos de la respuesta
 	})
 	.catch((error: AxiosError) => {
-			const status = error.response?.status || 500; // Valor por defecto
-			const msg = error.message;
+		const status = error.response?.status || 500; // Valor por defecto
+		const msg = error.message;
 
-			// Retornar un objeto de error
-			return {
-					status,
-					msg
-			};
+		// Retornar un objeto de error
+		return {
+			status,
+			msg
+		};
 	});
 };
 
 export const updateBudget = (token: string, id: number, budget: BudgetUpdate): Promise<BudgetOut | ErrorResponse> => {
 	const data = () => {
-			if (budget.period) {
-					return {
-							"category_id": budget.category_id,
-							"amount": budget.amount,
-							"period": {
-									"start_date": budget.period.start_date,
-									"end_date": budget.period.end_date
-							}
-					};
-			} else {
-					return {
-							"category_id": budget.category_id,
-							"amount": budget.amount,
-					};
-			}
+		if (budget.period) {
+			return {
+				"category_id": budget.category_id,
+				"amount": budget.amount,
+				"period": {
+					"start_date": budget.period.start_date,
+					"end_date": budget.period.end_date
+				}
+			};
+		} else {
+			return {
+				"category_id": budget.category_id,
+				"amount": budget.amount,
+			};
+		}
 	};
 
 	return axi.put(`/budgets/${id}`, data(), { headers: { 'Authorization': `Bearer ${token}` } })
 	.then(response => {
-			console.log(response.status);
-			return response.data; // Devuelve los datos de la respuesta
+		console.log(response.status);
+		return response.data; // Devuelve los datos de la respuesta
 	})
 	.catch((error: AxiosError) => {
-			const status = error.response?.status || 500; // Valor por defecto
-			const msg = error.message;
+		const status = error.response?.status || 500; // Valor por defecto
+		const msg = error.message;
 
-			// Retornar un objeto de error
-			return {
-					status,
-					msg
-			};
+		// Retornar un objeto de error
+		return {
+			status,
+			msg
+	};
 	});
 };
 
 export const getBudgetStatus = (token: string, id: number): Promise<BudgetStatus | ErrorResponse> => {
 	return axi.get(`/budgets/${id}/status`, { headers: { 'Authorization': `Bearer ${token}` } })
 	.then(response => {
-			console.log(response.status);
-			return response.data; // Devuelve los datos de la respuesta
+		console.log(response.status);
+		return response.data; // Devuelve los datos de la respuesta
 	})
 	.catch((error: AxiosError) => {
-			const status = error.response?.status || 500; // Valor por defecto
-			const msg = error.message;
+		const status = error.response?.status || 500; // Valor por defecto
+		const msg = error.message;
 
-			// Retornar un objeto de error
-			return {
-				status,
-				msg
+		// Retornar un objeto de error
+		return {
+			status,
+			msg
 		};
 	});
 };
@@ -122,17 +122,17 @@ export const getBudgetStatus = (token: string, id: number): Promise<BudgetStatus
 export const deleteBudget = (token: string, id: number): Promise<string | ErrorResponse> => {
 	return axi.delete(`/budgets/${id}`, { headers: { 'Authorization': `Bearer ${token}` } })
 	.then(response => {
-			console.log(response.status);
-			return response.statusText; // Devuelve el texto del estado de la respuesta
+		console.log(response.status);
+		return response.statusText; // Devuelve el texto del estado de la respuesta
 	})
 	.catch((error: AxiosError) => {
-			const status = error.response?.status || 500; // Valor por defecto
-			const msg = error.message;
+		const status = error.response?.status || 500; // Valor por defecto
+		const msg = error.message;
 
-			// Retornar un objeto de error
-			return {
-					status,
-					msg
-			};
+		// Retornar un objeto de error
+		return {
+			status,
+			msg
+		};
 	});
 };
--- frontend/web/app/src/services/budget.ts
+++ frontend/web/app/src/services/budget.ts
@@ -2,15 +2,15 @@ import {getBudgets, getBudget} from '@/api/budget'
 import { queryOptions } from '@tanstack/react-query'
 
 export function GetBudgets(token: string){
-    return queryOptions({
-        queryKey: ['budget'],
-        queryFn: () => getBudgets(token)
-    })
+  return queryOptions({
+    queryKey: ['budget'],
+    queryFn: () => getBudgets(token)
+  })
 }
 
 export function GetBudget(token: string, id: number){
-    return queryOptions({
-        queryKey: ['budget', id],
-        queryFn: () => getBudget(token, id)
-    })
+  return queryOptions({
+    queryKey: ['budget', id],
+    queryFn: () => getBudget(token, id)
+  })
 }
